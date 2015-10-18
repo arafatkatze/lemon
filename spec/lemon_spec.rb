d02f5ea878a00b4c7cc0c5a6a23fde43c5b571ea
loocash <lukaszkapica@gmail.com>
2015-10-18 16:18:25 +0200

    Add 2 failing tests

--- spec/lemon_spec.rb
+++ spec/lemon_spec.rb
@@ -8,10 +8,31 @@ describe Lemon do
 
 	context 'ListGraph' do
 
-		it 'creates a graph, adds 3 nodes and counts them' do
-			g = Lemon::ListGraph.new
-			3.times { g.addNode }
-			expect(Lemon.countNodes g).to eq(3)
+		before :each do
+			@g = Lemon::ListGraph.new
+		end
+
+		it 'adds nodes and properly counts them' do
+			3.times { @g.addNode }
+			expect(Lemon.countNodes @g).to eq(3)
+		end
+
+		it 'removes nodes and properly counts them' do
+			i, j, k = @g.addNode, @g.addNode, @g.addNode
+			expect(Lemon.countNodes @g).to eq(3)
+			@g.erase j
+			expect(Lemon.countNodes @g).to eq(2)
+			@g.erase k
+			expect(Lemon.countNodes @g).to eq(1)
+			@g.erase i
+			expect(Lemon.countNodes @g).to eq(0)
+		end
+
+		it 'clears itself' do
+			3.times { @g.addNode }
+			expect(Lemon.countNodes @g).to eq(3)
+			@g.clear
+			expect(Lemon.countNodes @g).to eq(0)
 		end
 
 	end
